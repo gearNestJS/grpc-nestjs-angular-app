84d1f384e7fb8d81f636733aa6f1654d0958f86a
Valery Semenenko <gearmobile@gmail.com>
2025-11-13 00:44:55 +0300

    feat: implement in controller and service

--- api/src/items/items.controller.ts
+++ api/src/items/items.controller.ts
@@ -1,4 +1,32 @@
 import { Controller } from '@nestjs/common';
+import {
+  CreateItemRequest,
+  Empty,
+  GetItemRequest,
+  Item,
+  Items,
+  ItemsServiceController,
+  ItemsServiceControllerMethods,
+} from './items';
+import { ItemsService } from './items.service';
+import { Observable } from 'rxjs';
 
 @Controller('items')
-export class ItemsController {}
+@ItemsServiceControllerMethods()
+export class ItemsController implements ItemsServiceController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  createItem(
+    request: CreateItemRequest,
+  ): Promise<Item> | Observable<Item> | Item {
+    return this.itemsService.createItem(request);
+  }
+
+  getItem(request: GetItemRequest): Promise<Item> | Observable<Item> | Item {
+    return this.itemsService.getItem(request);
+  }
+
+  streamItems(): Observable<Items> {
+    return this.itemsService.streamItems();
+  }
+}
--- api/src/items/items.service.ts
+++ api/src/items/items.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { CreateItemRequest, GetItemRequest, Item, Items } from './items';
+import { randomUUID } from 'node:crypto';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly items: Item[] = [];
+  private readonly itemsSubject = new Subject<Items>();
+
+  createItem(request: CreateItemRequest): Item {
+    const item: Item = {
+      id: randomUUID(),
+      ...request,
+    };
+    this.items.push(item);
+    this.itemsSubject.next({
+      items: [...this.items],
+    });
+
+    return item;
+  }
+
+  getItem(request: GetItemRequest): Item {
+    return (
+      this.items.find((item: Item) => item.id === request.id) || ({} as Item)
+    );
+  }
+
+  streamItems(): Observable<Items> {
+    return this.itemsSubject.asObservable();
+  }
+}
