cb3d4b8e43e7340edc7d318917f159bfdc2ec945
Valery Semenenko <gearmobile@gmail.com>
2025-11-13 00:08:03 +0300

    feat: add support proto and generate from proto

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'items',
+      protoPath: join(__dirname, 'items/items.proto'),
+      url: '0.0.0.0:9091',
+    },
+  });
+  await app.startAllMicroservices();
 }
 bootstrap();
